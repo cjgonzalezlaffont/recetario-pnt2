764e6c13a40b82f44efe89f3539663d96a7a2dc4
JonaCaucota <jonacau12@gmail.com>
2023-06-16 16:50:34 -0300

    footer, header y main terminados

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route , Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {SignIn} from "./components/login/SignIn";
 import {SignUp} from "./components/login/SignUp";
 import {Header} from "./components/header/Header";
+import {Home} from "./components/home/Home";
+import {Authenticator} from "./components/login/Authenticator";
+import {Footer} from "./components/footer/Footer";
 
 function App() {
     return (
-        <div className="App container bg-black">
+        <div className={'bg-warning'}>
             <Header></Header>
-            <div className="auth-wrapper">
-                <div className="auth-inner">
-                    <Routes>
-                        <Route exact path="/" element={<SignIn/>}/>
-                        <Route path="/sign-in" element={<SignIn/>}/>
-                        <Route path="/sign-up" element={<SignUp/>}/>
-                    </Routes>
-                </div>
-            </div>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/authenticator" element={<Authenticator/>}/>
+                    <Route path="/sign-in" element={<SignIn></SignIn>}/>
+                    <Route path="/sign-up" element={<SignUp></SignUp>}/>
+                </Routes>
+            </main>
+            <Footer></Footer>
+
         </div>
     );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,42 +1,45 @@
+import {Link} from "react-router-dom";
+import logo from "../../assets/logo.png";
+
 export const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary p-4">
+                <div className="container-fluid">
+                    <a className="navbar-brand me-5" href="#">
+                        <img src={logo} alt="Bootstrap" width="120" height="50"></img>
+                    </a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item me-5 ms-lg-5">
+                                <Link to={'/'} className={'text-decoration-none'}>
+                                    <button className={'nav-link'}><h5>Home</h5></button>
+                                </Link>
+                            </li>
+                            <li className="nav-item me-5 ">
+                                <Link to={'/'} className={'text-decoration-none'}>
+                                    <button className={'nav-link'}><h5>Link</h5></button>
+                                </Link>
+                            </li>
+                            <li className="nav-item me-5">
+                                <Link to={'/sign-in'} className={'text-decoration-none'}>
+                                    <button className={'nav-link'}><h5>Login</h5></button>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/sign-up'} className={'text-decoration-none'}>
+                                    <button className={'nav-link'}><h5>Sign Up</h5></button>
+                                </Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex col-4" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search for a recipe"
+                                   aria-label="Search"></input>
+                            <button className="btn btn-outline-success" type="submit">GO!</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
