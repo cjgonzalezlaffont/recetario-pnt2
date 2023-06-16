84a00bfd89cd3fc9a7cb624d73935b5bad10a5a1
JonaCaucota <jonacau12@gmail.com>
2023-06-16 12:11:43 -0300

    Agregado de header y ambas paginas, login y sign up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route , Routes} from 'react-router-dom';
+import {SignIn} from "./components/login/SignIn";
+import {SignUp} from "./components/login/SignUp";
+import {Header} from "./components/header/Header";
 
 function App() {
-  return (
-    <h1>Aca debe ir el tp xd</h1>
-  );
+    return (
+        <div className="App container bg-black">
+            <Header></Header>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <Routes>
+                        <Route exact path="/" element={<SignIn/>}/>
+                        <Route path="/sign-in" element={<SignIn/>}/>
+                        <Route path="/sign-up" element={<SignUp/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
