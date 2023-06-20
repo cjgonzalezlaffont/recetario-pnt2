8b743fdcf7dbf00a0b9dd8e568a7bc9b6155af93
César Javier Gonzalez Laffont <cjgonzalezlaffont@gmail.com>
2023-06-20 12:19:03 -0300

    Lista de recetas v.1 -  Route:  /recipes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import {Route, Routes} from 'react-router-dom';
-import {SignIn} from "./components/login/SignIn";
-import {SignUp} from "./components/login/SignUp";
-import {Header} from "./components/header/Header";
-import {Home} from "./components/home/Home";
-import {Authenticator} from "./components/login/Authenticator";
-import {Footer} from "./components/footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import { SignIn } from "./components/login/SignIn";
+import { SignUp } from "./components/login/SignUp";
+import { Header } from "./components/header/Header";
+import { Home } from "./components/home/Home";
+import { Authenticator } from "./components/login/Authenticator";
+import { Footer } from "./components/footer/Footer";
+import { RecipeList } from "./components/Recipes/RecipeList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <div className={'bg-warning'}>
-            <Header></Header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/authenticator" element={<Authenticator/>}/>
-                    <Route path="/sign-in" element={<SignIn></SignIn>}/>
-                    <Route path="/sign-up" element={<SignUp></SignUp>}/>
-                </Routes>
-            </main>
-            <Footer></Footer>
-
-        </div>
-    );
+  return (
+    <div className={"bg-warning"}>
+      <Header></Header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authenticator" element={<Authenticator />} />
+          <Route path="/sign-in" element={<SignIn></SignIn>} />
+          <Route path="/sign-up" element={<SignUp></SignUp>} />
+          <Route path="/recipes" element={<RecipeList />} />
+        </Routes>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,45 +1,59 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
 export const Header = () => {
-    return (
-        <header>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary p-4">
-                <div className="container-fluid">
-                    <a className="navbar-brand me-5" href="#">
-                        <img src={logo} alt="Bootstrap" width="120" height="50"></img>
-                    </a>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item me-5 ms-lg-5">
-                                <Link to={'/'} className={'text-decoration-none'}>
-                                    <button className={'nav-link'}><h5>Home</h5></button>
-                                </Link>
-                            </li>
-                            <li className="nav-item me-5 ">
-                                <Link to={'/'} className={'text-decoration-none'}>
-                                    <button className={'nav-link'}><h5>Link</h5></button>
-                                </Link>
-                            </li>
-                            <li className="nav-item me-5">
-                                <Link to={'/sign-in'} className={'text-decoration-none'}>
-                                    <button className={'nav-link'}><h5>Login</h5></button>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={'/sign-up'} className={'text-decoration-none'}>
-                                    <button className={'nav-link'}><h5>Sign Up</h5></button>
-                                </Link>
-                            </li>
-                        </ul>
-                        <form className="d-flex col-4" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search for a recipe"
-                                   aria-label="Search"></input>
-                            <button className="btn btn-outline-success" type="submit">GO!</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary p-4">
+        <div className="container-fluid">
+          <a className="navbar-brand me-5" href="#">
+            <img src={logo} alt="Bootstrap" width="120" height="50"></img>
+          </a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item me-5 ms-lg-5">
+                <Link to={"/"} className={"text-decoration-none"}>
+                  <button className={"nav-link"}>
+                    <h5>Home</h5>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav-item me-5 ">
+                <Link to={"/recipes"} className={"text-decoration-none"}>
+                  <button className={"nav-link"}>
+                    <h5>Recipes</h5>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav-item me-5">
+                <Link to={"/sign-in"} className={"text-decoration-none"}>
+                  <button className={"nav-link"}>
+                    <h5>Login</h5>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/sign-up"} className={"text-decoration-none"}>
+                  <button className={"nav-link"}>
+                    <h5>Sign Up</h5>
+                  </button>
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex col-4" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search for a recipe"
+                aria-label="Search"
+              ></input>
+              <button className="btn btn-outline-success" type="submit">
+                GO!
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
